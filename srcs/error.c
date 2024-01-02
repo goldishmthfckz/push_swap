92fe8f475d1f75bcbc79a0ad4c083c08c75ee729
goldishmthfckz <124591796+goldishmthfckz@users.noreply.github.com>
2024-01-02 18:26:14 +0100

    Update error.c

--- srcs/error.c
+++ srcs/error.c
@@ -1,18 +1,18 @@
 #include "../incl/push_swap.h"
 
 //checker s'il y a des doublons
-int	doublons(char **argv)
+int	doublons(char **av)
 {
 	int	i;
 	int	j;
 
 	i = 1;
-	while (argv[i])
+	while (av[i])
 	{
 		j = i + 1;
-		while (argv[j])
+		while (av[j])
 		{
-			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
+			if (ft_atoi(av[i]) == ft_atoi(av[j]))
 				return (1);
 			j++;
 		}
@@ -22,23 +22,40 @@ int	doublons(char **argv)
 }
 
 //checker s'il y a des lettres
-int	alpha(char **argv)
+int	alpha(char **av)
 {
 	int	i;
 	int	j;
 
 	i = 1;
-	while (argv[i])
+	while (av[i])
 	{
 		j = 0;
-		if (argv[i][0] == '-')
+		if (av[i][0] == '-')
 			j++;
-		while (argv[i][j])
+		while (av[i][j])
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			if (av[i][j] < '0' || av[i][j] > '9')
 				return (1);
 			j++;
 		}
+		i++;
+	}
+	return (0);
+}
+
+//checker si chaque av est ds la limite du long
+int	arglen(char **av)
+{
+	int	i;
+
+	i = 0;
+	while (av[i])
+	{
+		if (ft_atoi(av[i]) < -2147483648)
+			return (1);
+		else if (ft_atoi(av[i]) > 2147483647)
+			return (1);
 		i++;
 	}
 	return (0);
