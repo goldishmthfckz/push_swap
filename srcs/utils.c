83929d4dacd62f15cf7c8042d9f59cd328f5840a
goldishmthfckz <124591796+goldishmthfckz@users.noreply.github.com>
2024-01-02 17:21:47 +0100

    Update utils.c

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,5 +1,9 @@
 #include "../incl/push_swap.h"
 
+//derniere node
+//tri ou pas???
+//
+
 //renvoi de la derniere node
 pile	*lastnode(pile *list)
 {
@@ -8,4 +12,20 @@ pile	*lastnode(pile *list)
 	while (list->next)
 		list = list->next;
 	return (list);
+}
+
+//1 si triee, 0 sinon
+int	sorted(pile *a)
+{
+	int	i;
+
+	i = a->nbr;
+	while (a)
+	{
+		if (i > a->nbr)
+			return (0);
+		i = a->nbr;
+		a = a->next;
+	}
+	return (1);
 }
